fed0a9db7f134a94ee74a6987e24347008d20822
Muhammad Abdurrohman <abdurrohmanmuhammad123@gmail.com>
2022-06-04 14:37:17 +0700

    Callback function

--- index.js
+++ index.js
@@ -6,15 +6,35 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (arr, func) => {
+  const res = [];
+  if (func(arr)) {
+    res.push(func(arr));
+  }
+  return res;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (data) => {
+  const res = data.sort();
+  const hasil = [];
+  for (let i = 0; i < res.length; i++) {
+    hasil.push((i + ". " + res[i]).toString());
+  }
+  return hasil;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (data) => {
+  const res = data.sort().reverse();
+  const hasil = [];
+  for (let i = 0; i < res.length; i++) {
+    hasil.push((i + ". " + res[i]).toString());
+  }
+  return hasil;
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
